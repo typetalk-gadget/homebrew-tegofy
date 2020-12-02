2b8ed8f02c9566d2288a82d99b41f11fcc3fdef0
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2020-12-02 13:51:08 +0900

    Update tegofy.rb

--- tegofy.rb
+++ tegofy.rb
@@ -1,8 +1,8 @@
 class Tegofy < Formula
     desc "A command line tool for ego-search notify in Typetalk"
-    url "https://github.com/typetalk-gadget/tegofy/releases/download/v0.1.2/tegofy_v0.1.2_darwin_amd64.zip"
-    sha256 "6712f127a320be5f90e0d316c1c9f61b9085992fd660f1fd3f55a9ce0caf1bab"
-    version "v0.1.2"
+    url "https://github.com/typetalk-gadget/tegofy/releases/download/v0.1.3/tegofy_v0.1.3_darwin_amd64.zip"
+    sha256 "4efaa1104564d2eea248f72ce746faac729e5a6c40fd78217dbf2c891a798b6f"
+    version "v0.1.3"
     def install
         bin.install 'tegofy'
     end
